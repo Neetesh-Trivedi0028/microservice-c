7ba824a602089d2dfb6245bb6f297ce3a98c37c0
Neetesh Trivedi <neeteshtrivedi2002@gmail.com>
2024-11-16 19:46:30 +0530

    solving error

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const connectDB = require('./db');
+// const connectDB = require('./db');
 
 app.listen(3003, () => console.log("Microservice C running on port 3003"));
 // const microserviceA = connectDB("mongodb://localhost:27017/microserviceA");
--- db.js
+++ db.js
@@ -10,15 +10,40 @@ const connectDB = (uri, options = {}) => {
     return connection;
 };
 
-module.exports = connectDB;
-
 // Initialize connections for each database
-// const microserviceA = connectDB("mongodb://localhost:27017/microserviceA");
-// const microserviceB = connectDB("mongodb://localhost:27017/microserviceB");
-// const microserviceC = connectDB("mongodb://localhost:27017/microserviceC");
+const microserviceA = connectDB("mongodb://localhost:27017/microserviceA");
+const microserviceB = connectDB("mongodb://localhost:27017/microserviceB");
+const microserviceC = connectDB("mongodb://localhost:27017/microserviceC");
 
-// module.exports = {
-//     microserviceA,
-//     microserviceB,
-//     microserviceC,
+module.exports = {
+    microserviceA,
+    microserviceB,
+    microserviceC,
+};
+
+
+
+// const mongoose = require("mongoose");
+
+// const connectDB = (uri, options = {}) => {
+//     const connection = mongoose.createConnection(uri, options);
+
+//     connection.on("connected", () => console.log(`Connected to ${uri}`));
+//     connection.on("error", (err) => console.error(`Connection error on ${uri}:`, err));
+//     connection.on("disconnected", () => console.log(`Disconnected from ${uri}`));
+
+//     return connection;
 // };
+
+// module.exports = connectDB;
+
+// // Initialize connections for each database
+// // const microserviceA = connectDB("mongodb://localhost:27017/microserviceA");
+// // const microserviceB = connectDB("mongodb://localhost:27017/microserviceB");
+// // const microserviceC = connectDB("mongodb://localhost:27017/microserviceC");
+
+// // module.exports = {
+// //     microserviceA,
+// //     microserviceB,
+// //     microserviceC,
+// // };
