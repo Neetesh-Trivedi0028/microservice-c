51eb0664419068ef60d4bc7b0a0a68abe6cb6ba4
Neetesh Trivedi <neeteshtrivedi2002@gmail.com>
2024-11-16 23:32:56 +0530

    enhance code for global microservice

--- db/connectToDatabase.js
+++ db/connectToDatabase.js
@@ -4,21 +4,56 @@ const connections = {};
 
 /**
  * Connect to a database and cache the connection.
- * @param {String} dbURI - The MongoDB connection string.
- * @param {String} dbName - The name of the database.
+ * @param {String} connectionString - The full MongoDB connection string (e.g., "mongodb://127.0.0.1:27017/microserviceB").
  * @returns {Object} - The database connection.
  */
-const connectToDatabase = async (dbURI, dbName) => {
-    if (connections[dbName]) {
-        return connections[dbName];
+const connectToDatabase = async (connectionString) => {
+    if (connections[connectionString]) {
+        return connections[connectionString];
     }
-    const connection = await mongoose.createConnection(dbURI, {
-        dbName,
+
+    // Create and cache the new connection
+    const connection = await mongoose.createConnection(connectionString, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).asPromise();
-    connections[dbName] = connection;
+
+    connections[connectionString] = connection;
     return connection;
 };
 
 module.exports = connectToDatabase;
+
+
+
+
+
+
+// method 1:    for reference code this approach
+
+// const mongoose = require("mongoose");
+// const connections = {};
+
+/**
+ * Connect to a database and cache the connection.
+ * @param {String} dbURI - The MongoDB connection string.
+ * @param {String} dbName - The name of the database.
+ * @returns {Object} - The database connection.
+ */
+// const connectToDatabase = async (dbURI, dbName) => {
+//     if (connections[dbName]) {
+//         return connections[dbName];
+//     }
+//     const connection = await mongoose.createConnection(dbURI, {
+//         dbName,
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     }).asPromise();
+//     connections[dbName] = connection;
+//     return connection;
+// };
+
+// module.exports = connectToDatabase;
+
+
+// method 1:    for reference code this approach
--- index.js
+++ index.js
@@ -3,6 +3,35 @@ const userSchema = require("./models/User");
 const orderSchema = require("./models/Order");
 
 module.exports = {
+    /**
+     * Get the User model from the specified database.
+     * @param {String} connectionString - The full MongoDB connection string.
+     * @returns {Object} - The User model.
+     */
+    getUserModel: async (connectionString) => {
+        const connection = await connectToDatabase(connectionString);
+        return connection.model("User", userSchema);
+    },
+
+    /**
+     * Get the Order model from the specified database.
+     * @param {String} connectionString - The full MongoDB connection string.
+     * @returns {Object} - The Order model.
+     */
+    getOrderModel: async (connectionString) => {
+        const connection = await connectToDatabase(connectionString);
+        return connection.model("Order", orderSchema);
+    },
+};
+
+
+/* method 1:    for reference code this approach 
+
+const connectToDatabase = require("./db/connectToDatabase");
+const userSchema = require("./models/User");
+const orderSchema = require("./models/Order");
+
+module.exports = {
     getUserModel: async (dbURI, dbName) => {
         const connection = await connectToDatabase(dbURI, dbName);
         return userSchema(connection);
@@ -12,3 +41,5 @@ module.exports = {
         return orderSchema(connection);
     },
 };
+
+*/
