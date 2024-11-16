b534beb60aa7ef49085badd793fb30c5d2dbfa1c
Neetesh Trivedi <neeteshtrivedi2002@gmail.com>
2024-11-16 23:13:43 +0530

    success

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const connectToDatabase = require("./db/connectToDatabase");
 const userSchema = require("./models/User");
 const orderSchema = require("./models/Order");
-console.log("check is it available in folder as latest code ")
+
 module.exports = {
     getUserModel: async (dbURI, dbName) => {
         const connection = await connectToDatabase(dbURI, dbName);
